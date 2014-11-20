fa39477cdb7f7d3e3e54d1a0570b5fa37c9d1da6
imnotdannorton <dnorton@dailybreak.com>
2014-11-19 22:54:48 -0500

    yeah kid

---
+++ colaiuta/app/js/contants.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('vaterDotcom.controllers', []);
+ /*.controller('homepageController', ['$scope', function($scope) {
+    console.log("whoo home");
+  	$scope.features = ['WOO', 'HEY NOW', 'WHATS THAT?'];
+  }])
+  .controller('artistController', ['$scope', function($scope) {
+  	$scope.features = ['WOO', 'HEY NOW', 'WHATS THAT?'];
+  }])
+  .controller('productController', ['$scope', function($scope) {
+  	$scope.features = ['WOO', 'HEY NOW', 'WHATS THAT?'];
+  }])
+  .controller('featureController', ['$scope', function($scope) {
+  	$scope.features = ['WOO', 'HEY NOW', 'WHATS THAT?'];
+  }])
+  .controller('dealerController', ['$scope', function($scope) {
+  	$scope.features = ['WOO', 'HEY NOW', 'WHATS THAT?'];
+  }]);*/
+
+
+
---
+++ colaiuta/app/js/env.js
@@ -0,0 +1,5 @@
+ENV = {
+  ARTIST_IMAGE_PATH: 'http://vater.s3.amazonaws.com/artists/',
+  PRODUCT_IMAGE_PATH : 'http://vater.s3.amazonaws.com/products/',
+  PRODUCT_IMAGE_PATH_HIRES: 'http://vater.s3.amazonaws.com/products_fullres/'
+}
