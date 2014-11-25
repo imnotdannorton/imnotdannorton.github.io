4ab80cfb2d6248c48fda649565f52703e10cb864
imnotdannorton <dnorton@dailybreak.com>
2014-11-24 21:39:06 -0500

    err

---
+++ colaiuta/app/js/controllers/compareController.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('vaterDotcom').controller('CompareCtrl', ['$scope', '$rootScope', '$location', '$routeParams',  'resourcesService', 'PRODUCT_IMAGE_PATH', function($scope, $rootScope, $location, $routeParams, resourcesService, PRODUCT_IMAGE_PATH) {
+  	
+    $scope.prodId = $routeParams.id;
+
+    $scope.compareItems = [];
+    $scope.compareObjects = [];
+    
+    $scope.imgPath = PRODUCT_IMAGE_PATH;
+    $scope.imgPathLrg = PRODUCT_IMAGE_PATH + '_fullres';
+    $scope.showZoom = false;
+    
+    /*$scope.buildComparison = function(array){
+      console.log(array);
+      angular.forEach(array, function(value, key){
+        resourcesService.fetchItem('product', value);
+      });
+    }*/
+    
+
+    console.log($routeParams);
+    if($routeParams){
+      angular.forEach($routeParams, function(e, i){
+          console.log(e);
+          resourcesService.fetchItem('product', e);
+        });
+       // $scope.buildComparison($scope.compareItems);
+    }
+    $scope.$on('productSuccess', function(event, data){
+          $scope.compareObjects.push(data);
+          console.log( $scope.compareObjects.length);
+      });
+
+    // dont think i need this actually
+    $rootScope.$on('artistSuccess', function(event, data){
+      for (var i = 0; i < $scope.product.artists.length; i++) {
+        if($scope.product.artists[i].id == data.id){
+          $scope.product.artists[i] = data;
+          $scope.product.artists[i].image = data.images[0].url;
+          console.log($scope.product.artists[i]);
+        }
+      };
+    });
+
+    // compare products
+    $scope.processArtists = function(array){
+     for (var i = 0; i < array.length; i++) {
+       artistService.fetchItem('artist', array[i].id);
+     };
+    }
+    $scope.toCentimeters = function(value){
+      var cm = parseFloat(value*2.54).toFixed(2);
+      return cm;
+    }
+ }]);
