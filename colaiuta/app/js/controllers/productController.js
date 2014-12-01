b24537fd01fa47287de715b2ca0cae2317b71fef
Dan Norton <dnorton@dailybreak.com>
2014-12-01 09:36:36 -0500

    sort and compare updates

--- colaiuta/app/js/controllers/productController.js
+++ colaiuta/app/js/controllers/productController.js
@@ -5,15 +5,15 @@
 angular.module('vaterDotcom').controller('ProductCtrl', ['$scope', '$rootScope', '$location', '$routeParams',  'resourcesService', 'PRODUCT_IMAGE_PATH', function($scope, $rootScope, $location, $routeParams, resourcesService, PRODUCT_IMAGE_PATH) {
   	
     $scope.prodId = $routeParams.id;
-
+    $scope.sortby = 'name'
     $scope.compareItems = [];
     $scope.prodLoading = $rootScope.loading;
     $scope.imgPath = PRODUCT_IMAGE_PATH;
     $scope.imgPathLrg = PRODUCT_IMAGE_PATH + '_fullres';
     $scope.showZoom = false;
-    $scope.compareLink = '/app/#/compare?';
-  
-   
+    $scope.ascending = true;
+    
+
   	if(typeof $scope.prodId !== 'undefined'){
   		resourcesService.fetchItem('product', $scope.prodId);
      
@@ -24,13 +24,24 @@ angular.module('vaterDotcom').controller('ProductCtrl', ['$scope', '$rootScope',
 
     $scope.$on('productSuccess', function(event, data){
         $scope.product = data;
+        console.log($scope.product);
         if ($scope.product.artists.length>0) {
           $scope.processArtists($scope.product.artists);
         };
+        // Change image path to default to wood
+
+        if($scope.product.product_code.indexOf('Nylon') > -1){
+          $scope.product.images[0].name += '_w';
+          $scope.product.product_code.replace('<br>', '/');
+          //console.log($scope.product.images[0].name); 
+        }
+        window.document.title = 'Vater Percussion | ' + $scope.product.name;
       });
     $scope.$on('productsSuccess', function(event, data){
         $scope.products = data;
+        window.document.title = 'Vater Percussion | Products'
         console.log(data);
+
       });
 
     $rootScope.$on('artistSuccess', function(event, data){
@@ -47,24 +58,38 @@ angular.module('vaterDotcom').controller('ProductCtrl', ['$scope', '$rootScope',
       console.log('from: '+oldVal);
     });
     // compare products
+    $scope.swapSort = function(string){
+      if (string.indexOf('-')>-1) {
+        $scope.sortby = string.replace('-', '');
+        $scope.ascending = true;
+        $scope.descending = false;
+      }else{
+        $scope.sortby = '-'+string;
+        $scope.ascending = false;
+        $scope.descending = true;
+      };
+      console.log($scope.sortby);
+    }
     $scope.compareThis = function(product){
       $scope.compareItems.push(product);
-      var idString = ($scope.compareItems.length == 1) ? 'id'+$scope.compareItems.length+'=' : '&id'+$scope.compareItems.length+'=';
-      $scope.compareLink = $scope.compareLink +idString+product.id;
-
+      $scope.buildComparison();
     }
-    $scope.viewComparison = function(){
+    $scope.buildComparison = function(){
+      $scope.compareLink = '/app/#/compare?';
+      for (var i = 0; i < $scope.compareItems.length; i++) {
+      var idString = (i+1 == 1) ? 'id'+(i+1)+'=' : '&id'+(i+1)+'=';
+      $scope.compareLink = $scope.compareLink +idString+$scope.compareItems[i].id;
+      };
+      console.log( $scope.compareLink);
 
     }
     $scope.removeItem = function(id){
       console.log(id);
+      $scope.compareItems.splice(id, 1);
+
       console.log($scope.compareItems);
-      $scope.compareItems = $scope.compareItems.splice(id, 1);
-      for (var i = 0; i < $scope.compareItems.length; i++) {
-        $scope.compareThis(compareItems[i]);
-      };
-      console.log( $scope.compareLink);
-      $scope.$apply();
+      $scope.buildComparison();
+     // $scope.$apply();
     }
     $scope.processArtists = function(array){
      for (var i = 0; i < array.length; i++) {
