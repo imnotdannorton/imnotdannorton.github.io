7bd92770f1c76fac1f1ed22a8004f8e26407517b
Dan Norton <dnorton@dailybreak.com>
2014-12-08 16:06:59 -0500

    woo artists

--- colaiuta/app/js/controllers/headerController.js
+++ colaiuta/app/js/controllers/headerController.js
@@ -9,7 +9,15 @@ angular.module('vaterDotcom').controller('headerController', ['$scope', '$locati
 	//console.log($location.path());
 	//console.log($route.current.params);
 	$scope.setActiveTab = function(tab){
+	$scope.redirect(tab);
+	$location.search('');
 	$('#header ul li a').removeClass('active');
 	$('#'+tab).addClass('active');
+	//console.log($location.path());
+	$('#mobileNav.active').removeClass('active'); // move mobile nav out
   }
+  $scope.redirect = function(path){
+  	$location.path(path);
+  }
+ 
 }]);
